806c01e7e3196f5656f5794318bc7d3a7e473c64
SanjayPrasad1 <132146437+SanjayPrasad1@users.noreply.github.com>
2026-03-12 13:45:34 +0530

    Delete src directory

--- src/main/java/com/firstVersion/RevPay/service/AnalyticsService.java
+++
@@ -1,7 +0,0 @@
-package com.firstVersion.RevPay.service;
-
-import com.firstVersion.RevPay.dto.DashboardDTO;
-
-public interface AnalyticsService {
-    DashboardDTO getUserDashboard(String email);
-}
